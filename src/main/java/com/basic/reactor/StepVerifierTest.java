66cdb6431e5ec3a620c9c2dffb107c48e5e09c04
awj0415 <awj0415@gmail.com>
2023-03-09 21:23:10 +0900

    stepVerifier 예제

--- src/main/java/com/basic/reactor/StepVerifierTest.java
+++ src/main/java/com/basic/reactor/StepVerifierTest.java
@@ -1,15 +1,18 @@
 package com.basic.reactor;
 
-import org.springframework.util.Assert;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.function.Supplier;
 
 public class StepVerifierTest {
 
     public static void main(String[] args) {
 //        expectFooBarComplete();
 //        expectFooBarError();
-        expectSkylerJessiComplete();
+//        expectSkylerJessiComplete();
+        expect10Elements();
     }
 
     static void expectFooBarComplete() {
@@ -48,5 +51,25 @@ public class StepVerifierTest {
         }
     }
 
+    static void expect10Elements() {
+        Flux<Long> take10 = Flux.interval(Duration.ofMillis(100))
+                .take(10);
+
+        StepVerifier.create(take10)
+                .expectNextCount(10)
+                .verifyComplete();
+    }
+
+    static void expect3600Elements(Supplier<Flux<Long>> supplier) {
+        /*
+            1초에 하나씩 숫자 데이터를 만든다고 했을때
+            1시간을 감아서
+            3600개가 되는지 확인하는 예제
+         */
+        StepVerifier.withVirtualTime(supplier)
+                .thenAwait(Duration.ofHours(1))
+                .expectNextCount(3600)
+                .verifyComplete();
+    }
 
 }
