0331e96e2a4821dde25444a317a4cf53e9953f4a
awj0415 <awj0415@gmail.com>
2023-03-10 20:53:40 +0900

    flatMapSequential

--- src/main/java/com/basic/reactor/TransformOperator.java
+++ src/main/java/com/basic/reactor/TransformOperator.java
@@ -3,12 +3,18 @@ package com.basic.reactor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class TransformOperator {
 
     public static void main(String[] args) throws InterruptedException {
 //        capitalizeOne();
 //        capitalizeMany();
-        asyncCapitalizeMany();
+//        asyncCapitalizeMany();
+        flatMapSequentialTest();
     }
 
     static void capitalizeOne() {
@@ -39,6 +45,28 @@ public class TransformOperator {
         public String getUsername() {
             return username;
         }
+    }
+
+    static void flatMapSequentialTest() {
+        Flux.just("a","b","c",
+                  "d","e","f",
+                  "g","h","i")
+                .window(3)
+//                .flatMap(f -> f.map(TransformOperator::toUpperCase)) // 딜레이 있는채로
+//                .flatMap(f -> f.map(TransformOperator::toUpperCase).subscribeOn(parallel())) // 병렬로 > 딜레이 없어짐 > 단 섞여있음
+//                .concatMap(f -> f.map(TransformOperator::toUpperCase).subscribeOn(parallel())) // 순서유지 > 다시 딜레이가 생김
+                .flatMapSequential(f -> f.map(TransformOperator::toUpperCase).subscribeOn(parallel())) // 병렬로&순서유지 > 딜레이 없어짐
+                .doOnNext(System.out::println)
+                .blockLast();
+    }
+
+    static List<String> toUpperCase(String s) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(s.toUpperCase(), Thread.currentThread().getName());
     }
 
 }
